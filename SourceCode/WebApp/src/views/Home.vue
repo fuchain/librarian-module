075dc41c46429a0fe27ed50f006ea362b8573176
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-05-21 15:41:37 +0700

    improve style full page

--- SourceCode/WebApp/src/views/Home.vue
+++ SourceCode/WebApp/src/views/Home.vue
@@ -3,9 +3,16 @@
     <div
       class="vx-col flex items-center justify-center flex-col sm:w-1/2 md:w-3/5 lg:w-3/4 xl:w-1/2 mx-auto text-center"
     >
-      <img src="@/assets/images/pages/login.png" alt="graphic-maintenance" class="mx-auto mb-4">
-      <h1 class="mb-6 text-5xl d-theme-heading-color">Thư viện FPT Univeristy</h1>
-      <p class="mb-6 d-theme-text-inverse">Trang này đang được bảo trì, vui lòng quay lại sau.</p>
+      <img
+        src="@/assets/images/pages/login.png"
+        alt="graphic-maintenance"
+        class="mx-auto mb-4"
+        style="max-width: 250px;"
+      >
+      <h1 class="mb-6 pl-4 pr-4 text-4xl d-theme-heading-color">Thư viện FPT Univeristy</h1>
+      <p
+        class="mb-6 d-theme-text-inverse ml-4 mr-8"
+      >Trang này đang được bảo trì, vui lòng quay lại sau.</p>
       <vs-button size="large" to="/">Quay về thư viện</vs-button>
     </div>
   </div>
