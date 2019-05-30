eece70b3f9de522f89d6b717ca48007fad4281e4
pdoan4 <pdoan4@dxc.com>
2019-05-30 23:52:01 +0700

    [PHONGDV] remove response.xml because of in jdk 12 not support that class

--- SourceCode/WebServer/src/main/java/com/fpt/edu/controller/RequestController.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/controller/RequestController.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
-
 @RestController
 @RequestMapping("requests")
 public class RequestController extends BaseController {
