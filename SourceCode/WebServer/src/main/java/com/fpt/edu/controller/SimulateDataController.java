cb462e9426f6aa3c7e09b5a168e4751642d3604a
pdoan4 <pdoan4@dxc.com>
2019-05-30 14:47:27 +0700

    [PHONGDV] Update swagger

--- SourceCode/WebServer/src/main/java/com/fpt/edu/controller/SimulateDataController.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/controller/SimulateDataController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
--- SourceCode/WebServer/src/main/java/com/fpt/edu/controller/UserController.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.fpt.edu.controller;
 
+import com.fpt.edu.constant.Constant;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.fpt.edu.entities.User;
 import com.fpt.edu.services.UserServices;
@@ -20,25 +20,16 @@ public class UserController {
 	private UserServices userServices;
 	
 	
-	@ApiOperation(value = "View a list of available products", response = String.class)
-	@ApiResponses(value = {
-		    @ApiResponse(code = 200, message = "Successfully retrieved list"),
-		    @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-		    @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-		    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-		})
-
-	@RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json")
-	public String test() throws Exception {
-		User u = new User();
-		u.setUsername("phongdv");
-		u.setPassword("12345678");
-		JSONObject result = new JSONObject();
-		result.put("Message",userServices.save(u) );
-		throw new Exception("ec ");
+	@RequestMapping(value = "/{userId}/books/addABook",method = RequestMethod.PATCH, produces = Constant.APPLICATION_JSON)
+	public ResponseEntity<User> AddBooktoUser (@PathVariable String userId, @RequestBody String body){
 
 
-//		return result.toString();
+
+		return null;
 	}
+
+
+
+
 
 }
