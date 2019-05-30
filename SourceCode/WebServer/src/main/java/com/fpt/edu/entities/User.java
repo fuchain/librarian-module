4a9ae961630118222ded6b9e8342138d752353e2
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-05-30 20:10:26 +0700

    add jwt login

--- SourceCode/WebServer/src/main/java/com/fpt/edu/entities/User.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/entities/User.java
@@ -7,18 +7,15 @@ import javax.persistence.*;
 @Entity
 @Table(name="tbl_user")
 public class User {
-
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name="username",unique=true)
 	private String username;
-	
-	
+
 	@Column(name="password")
 	private String password;
-	
 	
 	@ManyToMany(cascade={CascadeType.ALL})
 	@JoinTable(name="user_role",joinColumns={@JoinColumn(name="user_id")},inverseJoinColumns={@JoinColumn(name="role_id")})
@@ -39,47 +36,31 @@ public class User {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getUsername() {
 		return username;
 	}
 
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	public List<Role> getRoles() {
 		return roles;
 	}
 
-
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
---
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/services/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.fpt.edu.services;
+
+import com.fpt.edu.entities.User;
+import com.fpt.edu.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        User user = null;
+
+        if (optionalUser.isPresent()) {
+            user = optionalUser.get();
+        }
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
+    }
+}
