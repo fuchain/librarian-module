ae819ab9b3e970493830657dcdc3f0fcd1691797
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-06-01 00:19:47 +0700

    update login auth google

--- SourceCode/WebServer/src/main/java/com/fpt/edu/security/SecurityConstants.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.fpt.edu.security;
 
 public class SecurityConstants {
     public static final String SECRET = "Capstone";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final int EXPIRATION_TIME = 86400; // 1 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
