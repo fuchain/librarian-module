c202731b94ebafca3df8feba9eefdeb757630bc8
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-06-03 14:28:44 +0700

    fix authen contorller ispresent to isempty

--- SourceCode/WebServer/src/main/java/com/fpt/edu/controller/AuthenticationController.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/controller/AuthenticationController.java
@@ -72,7 +72,7 @@ public class AuthenticationController {
             Optional<User> loggedUser = userServices.findUserByEmail(email);
 
             // If email is not in database
-            if (loggedUser.isPresent()) {
+            if (loggedUser.isEmpty()) {
                 return new ResponseEntity<>("User is not in database", HttpStatus.BAD_REQUEST);
             }
 
