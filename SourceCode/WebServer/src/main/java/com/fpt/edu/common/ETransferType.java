99977e1587868926e497f05e66c29e302206aa4a
toquoccuong <cuong.to@wisky.vn>
2019-06-05 11:33:55 +0700

    [CuongTq] comment on enum classes

--- SourceCode/WebServer/src/main/java/com/fpt/edu/common/ETransferType.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/common/ETransferType.java
@@ -1,7 +1,12 @@
 package com.fpt.edu.common;
 
+// The transfer type indicates the type of transfer
 public enum ETransferType {
+
+    // If the returner returns a book, the type of transfer is "retuner",
     RETURNER(1),
+
+    // If the borrower receives a book, the type of transfer is "receiver".
     RECEIVER(2);
 
     private int value;
