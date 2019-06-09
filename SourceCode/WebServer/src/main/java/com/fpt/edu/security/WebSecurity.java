4744ab1bc0fd7a2303bda4cabaa2e1505a54097a
PHONGDV <phongdvse61654@gmail.com>
2019-06-10 00:27:25 +0700

    [PHONGDV] Disable debugin security controller

--- SourceCode/WebServer/src/main/java/com/fpt/edu/security/WebSecurity.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/security/WebSecurity.java
@@ -15,7 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity()
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	private final UserDetailsServiceImpl userDetailsService;
 
