7819dfaa75898bf3e34f4b1307888e8885be6dfc
licolsan <phamhoanglinh198@gmail.com>
2019-06-09 14:35:58 +0700

    Refactor code of LibrarianController

--- SourceCode/WebServer/src/main/java/com/fpt/edu/controller/LibrarianController.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/controller/LibrarianController.java
@@ -29,9 +29,9 @@ public class LibrarianController extends BaseController {
 
 	@Autowired
 	public LibrarianController(UserServices userServices,
-							   BookDetailsServices bookDetailsServices,
-							   BookServices bookServices,
-							   BigchainTransactionServices bigchainTransactionServices) {
+	                           BookDetailsServices bookDetailsServices,
+	                           BookServices bookServices,
+	                           BigchainTransactionServices bigchainTransactionServices) {
 		this.userServices = userServices;
 		this.bookDetailsServices = bookDetailsServices;
 		this.bookServices = bookServices;
@@ -41,16 +41,23 @@ public class LibrarianController extends BaseController {
 
 	@ApiOperation("Get all users")
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> getAllUsers(@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE+"") int page, @RequestParam(name = "size", required = false,defaultValue = Constant.DEFAULT_OFFSET+"") int size ) {
-		Pageable pageable = PageRequest.of(page-1,size);
+	public ResponseEntity<List<User>> getAllUsers(
+		@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE + "") int page,
+		@RequestParam(name = "size", required = false, defaultValue = Constant.DEFAULT_OFFSET + "") int size
+	) {
+		Pageable pageable = PageRequest.of(page - 1, size);
 		List<User> userList = userServices.getAllUsers(pageable);
 		return new ResponseEntity<>(userList, HttpStatus.OK);
 	}
 
 	@ApiOperation("Get current book list by user id")
 	@GetMapping("/users/{id}/books")
-	public ResponseEntity<List<Book>> getBookByUserId(@PathVariable Long id,@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE+"") int page, @RequestParam(name = "size", required = false,defaultValue = Constant.DEFAULT_OFFSET+"") int size ) {
-		Pageable pageable = PageRequest.of(page-1,size);
+	public ResponseEntity<List<Book>> getBookByUserId(
+		@PathVariable Long id,
+		@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE + "") int page,
+		@RequestParam(name = "size", required = false, defaultValue = Constant.DEFAULT_OFFSET + "") int size
+	) {
+		Pageable pageable = PageRequest.of(page - 1, size);
 		userServices.getByUserId(id);
 		List<Book> bookList = userServices.getCurrentBookListOfUser(id);
 		return new ResponseEntity<>(bookList, HttpStatus.OK);
@@ -68,27 +75,31 @@ public class LibrarianController extends BaseController {
 	}
 
 	@ApiOperation(value = "Get list of all book details", response = List.class)
-	// need to identify specific class
 	@GetMapping("/book_details")
-	public ResponseEntity<List<BookDetail>> getListBookDetails(@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE+"") int page, @RequestParam(name = "size", required = false,defaultValue = Constant.DEFAULT_OFFSET+"") int size ) {
-		// Do we need authentication here???
-		Pageable pageable = PageRequest.of(page-1,size);
+	public ResponseEntity<List<BookDetail>> getListBookDetails(
+		@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE + "") int page,
+		@RequestParam(name = "size", required = false, defaultValue = Constant.DEFAULT_OFFSET + "") int size
+	) {
+		Pageable pageable = PageRequest.of(page - 1, size);
 		return new ResponseEntity<>(bookDetailsServices.getAllBookDetails(pageable), HttpStatus.OK);
 	}
 
 	@ApiOperation(value = "Get list instances of a book detail", response = List.class)
-	// need to identify specific class
 	@GetMapping("/book_details/{bookdetail_id}/books")
-	public ResponseEntity<List<Book>> getListBookInstances(@PathVariable("bookdetail_id") Long bookDetailId,@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE+"") int page, @RequestParam(name = "size", required = false,defaultValue = Constant.DEFAULT_OFFSET+"") int size ) {
-		// Do we need authentication here???
-		Pageable pageable = PageRequest.of(page-1,size);
-		return new ResponseEntity<>(bookServices.getListBookByBookDetailId(bookDetailId,pageable), HttpStatus.OK);
+	public ResponseEntity<List<Book>> getListBookInstances(
+		@PathVariable("bookdetail_id") Long bookDetailId,
+		@RequestParam(name = "page", required = false, defaultValue = Constant.DEFAULT_PAGE + "") int page,
+		@RequestParam(name = "size", required = false, defaultValue = Constant.DEFAULT_OFFSET + "") int size
+	) {
+		Pageable pageable = PageRequest.of(page - 1, size);
+		return new ResponseEntity<>(bookServices.getListBookByBookDetailId(bookDetailId, pageable), HttpStatus.OK);
 	}
 
 	@ApiOperation(value = "Get history of book instance", response = Book.class)
 	@GetMapping("/books/{book_id}")
-	public ResponseEntity<Book> getHistoryOfBookInstance(@PathVariable("book_id") Long bookId) throws Exception {
-		// Do we need authentication here???
+	public ResponseEntity<Book> getHistoryOfBookInstance(
+		@PathVariable("book_id") Long bookId
+	) throws Exception {
 		Book book = bookServices.getBookById(bookId);
 		book.setBcTransactions(
 			bigchainTransactionServices.getTransactionsByAssetId(
