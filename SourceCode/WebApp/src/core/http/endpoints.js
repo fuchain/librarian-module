92ca60f170a1dac50b9b5d0f55c2eed427f3e96a
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-06-12 17:36:44 +0700

    ID:10T Error

--- SourceCode/WebApp/src/core/http/endpoints.js
+++ SourceCode/WebApp/src/core/http/endpoints.js
@@ -2,7 +2,7 @@ const API_BASE_URL =
   process.env.VUE_APP_API_BASE_URL || "https://lapi.fptu.tech";
 
 const API_BETA_URL =
-  process.env.VUE_APP_API_BETA_URL || "https://bapi.fptu.tech";
+  process.env.VUE_APP_API_BETA_URL || "https://napi.fptu.tech";
 
 export default {
   baseUrl: API_BASE_URL + "/api/v1",
