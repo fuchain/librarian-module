a34551176d15ebac91421b7dbd3a66f93e1afe27
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-06-12 17:28:33 +0700

    changed things...

--- Deployment/build-docker-image/webapp.sh
+++ Deployment/build-docker-image/webapp.sh
@@ -3,7 +3,7 @@ ssh root@ssh.fptu.tech <<EOF
     cd /home/capstone/librarian-module/SourceCode/WebApp || exit 1
     git checkout . || exit 1
     git pull origin develop
-    sudo docker build -t librarian-webapp . || exit 1
+    docker build -t librarian-webapp . || exit 1
     docker stop librarian-webapp || exit 1
     docker rm librarian-webapp || exit 1
     docker run -d --name librarian-webapp -p 3000:80 librarian-webapp:latest || exit 1
--- Deployment/build-docker-image/webserver.sh
+++ Deployment/build-docker-image/webserver.sh
@@ -3,7 +3,7 @@ ssh root@ssh.fptu.tech <<EOF
     cd /home/capstone/librarian-module/SourceCode/WebServer
     git checkout . || exit 1
     git pull origin develop
-    sudo docker build -t librarian-webserver . || exit 1
+    docker build -t librarian-webserver . || exit 1
     docker stop librarian-webserver || exit 1
     docker rm librarian-webserver || exit 1
     docker run -d --name librarian-webserver -p 5000:8080 librarian-webserver:latest || exit 1
