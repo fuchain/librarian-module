e7cdea04411856d372455af75b96f962872ab3c2
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-06-17 02:15:54 +0700

    Remove unused routes

--- SourceCode/NodeServer/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import controller from "@controllers/auth.controller";
-
-const router = express.Router();
-
-router.route("/login").post(controller.login);
-
-export default router;
