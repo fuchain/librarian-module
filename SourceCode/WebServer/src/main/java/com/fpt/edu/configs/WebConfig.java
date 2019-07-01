2c7cf6af19c1d2b87b24609c1292ade83379f161
licolsan <phamhoanglinh198@gmail.com>
2019-07-01 13:09:26 +0700

    [LinhPH] Format WebConfig

--- SourceCode/WebServer/src/main/java/com/fpt/edu/configs/WebConfig.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/configs/WebConfig.java
@@ -8,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://library.fptu.tech", "https://thuvienfu.com",
-                        "http://dev.fptu.tech", "https://dev.fptu.tech", "http://dev.fptu.tech:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE").exposedHeaders("Authorization").allowCredentials(true);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins(
+				"http://localhost:3000", "https://library.fptu.tech", "https://thuvienfu.com",
+				"http://dev.fptu.tech", "https://dev.fptu.tech", "http://dev.fptu.tech:3000")
+			.allowedMethods("GET", "POST", "PUT", "DELETE").exposedHeaders("Authorization")
+			.allowCredentials(true);
+	}
 }
