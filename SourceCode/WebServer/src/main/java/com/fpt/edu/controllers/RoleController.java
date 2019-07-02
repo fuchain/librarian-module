7448398272a0ff072117472c836e9ad011ac2b91
ToQuocCuong <toquoccuong55@gmail.com>
2019-07-02 16:24:40 +0700

    System update book status to locked, prevent returner to make a return request

--- SourceCode/WebServer/src/main/java/com/fpt/edu/controllers/RoleController.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/controllers/RoleController.java
@@ -1,5 +1,29 @@
 package com.fpt.edu.controllers;
 
-public class RoleController {
+import com.fpt.edu.common.helpers.ImportHelper;
+import com.fpt.edu.common.request_queue_simulate.PublishSubscribe;
+import com.fpt.edu.common.request_queue_simulate.RequestQueueManager;
+import com.fpt.edu.services.*;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/roles")
+public class RoleController extends BaseController {
+
+	public RoleController(UserServices userServices, BookDetailsServices bookDetailsServices, BookServices bookServices, ImportHelper importHelper, MatchingServices matchingServices, RequestServices requestServices, TransactionServices transactionServices, PublishSubscribe publishSubscribe, RequestQueueManager requestQueueManager, NotificationService notificationService) {
+		super(userServices, bookDetailsServices, bookServices, importHelper, matchingServices, requestServices, transactionServices, publishSubscribe, requestQueueManager, notificationService);
+	}
+
+	@ApiOperation(value = "Get role by id", response = String.class)
+	@GetMapping("{id}")
+	public ResponseEntity<String> getById(@PathVariable Long id) {
+
+		return new ResponseEntity<>("", HttpStatus.OK);
+	}
 }
