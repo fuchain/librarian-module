130d67d226df7156db3097838ca78f6be5761b5c
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-04 01:51:25 +0700

    move your body every every body

--- SourceCode/WebApp/src/core/socket/redirect.js
+++ SourceCode/WebApp/src/core/socket/redirect.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 import $http from "@http";
+import $auth from "@auth";
 
 export default function redirect(notification) {
   if (notification.id) {
@@ -11,16 +12,32 @@ export default function redirect(notification) {
   }
 
   if (notification.type) {
-    switch (notification.type) {
-      case "keeping":
-        window.vue.$router.push("/books/keeping");
-        break;
-      case "returning":
-        window.vue.$router.push("/books/returning");
-        break;
-      case "requesting":
-        window.vue.$router.push("/books/requesting");
-        break;
+    if (!$auth.isAdmin()) {
+      switch (notification.type) {
+        case "keeping":
+          window.vue.$router.push("/books/keeping");
+          break;
+        case "returning":
+          window.vue.$router.push("/books/returning");
+          break;
+        case "requesting":
+          window.vue.$router.push("/books/requesting");
+          break;
+      }
+    } else {
+      if (notification.type.includes("bookinstance")) {
+        const arr = notification.type.split(":");
+
+        if (!isNaN(arr[1])) {
+          $http.get(`${$http.baseUrl}/books/${arr[1]}`).then(response => {
+            const id = response.data.bookDetail.id;
+
+            window.vue.$router.push(
+              `librarian/book-details-manage/${id}/${arr[1]}`
+            );
+          });
+        }
+      }
     }
   }
 }
