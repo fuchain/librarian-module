e7d647b509192e93ac8ead114d2de22b48a24c99
ToQuocCuong <toquoccuong55@gmail.com>
2019-07-06 19:26:07 +0700

    [CuongTQ] add API getBookDetailByID

--- SourceCode/WebServer/src/main/java/com/fpt/edu/common/helpers/GarbageJob.java
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/common/helpers/GarbageJob.java
@@ -21,7 +21,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class SchedulerJob implements Job {
+public class GarbageJob implements Job {
 
 	private final Logger LOGGER = LogManager.getLogger(getClass());
 	private RequestServices requestServices;
---
+++ SourceCode/WebServer/src/main/java/com/fpt/edu/common/helpers/SyncJob.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.common.helpers;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class SyncJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+
+	}
+}
