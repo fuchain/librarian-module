3e35f7b13cb856555e7998d307280504015489c2
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-07 00:29:30 +0700

    Update transfer service

--- SourceCode/BlockchainServer/src/routes/index.js
+++ SourceCode/BlockchainServer/src/routes/index.js
@@ -3,6 +3,7 @@ import testRoutes from "@routes/test.route";
 
 import authenticationRoutes from "@routes/authentication.route";
 import userRoutes from "@routes/user.route";
+import transferRoutes from "@routes/transfer.route";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get("/status", (_, res) => res.send("Server is up!"));
 router.use("/test", testRoutes);
 router.use("/auth", authenticationRoutes);
 router.use("/user", userRoutes);
+router.use("/transfer", transferRoutes);
 
 export default router;
