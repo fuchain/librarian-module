40a0acbfe28e41bd2af70909e7fab1bedd2c753a
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-06 17:17:24 +0700

    Remove redundant controller

--- SourceCode/BlockchainServer/src/controllers/authentication.controller.js
+++ SourceCode/BlockchainServer/src/controllers/authentication.controller.js
@@ -8,13 +8,11 @@ async function verifyKeyPairEmail(req, res) {
     const body = req.body;
 
     try {
-        const created = await keypairService.verifyKeyPairEmail(
+        const { token, status } = await keypairService.verifyKeyPairEmail(
             body.token,
             body.public_key
         );
-        res.status(201).send({
-            message: created ? "created" : "verfied"
-        });
+        res.status(201).send({ token, status });
     } catch (err) {
         res.status(422);
         res.send({
@@ -23,21 +21,4 @@ async function verifyKeyPairEmail(req, res) {
     }
 }
 
-async function googleAuthentication(req, res) {
-    const body = req.body;
-
-    try {
-        const { token, status } = await keypairService.verifyKeyPairEmail(
-            body.token,
-            body.public_key
-        );
-        res.status(200).send({ token, status });
-    } catch (err) {
-        res.status(422);
-        res.send({
-            message: err.toString()
-        });
-    }
-}
-
-export default { randomKeypair, verifyKeyPairEmail, googleAuthentication };
+export default { randomKeypair, verifyKeyPairEmail };
--- SourceCode/BlockchainServer/src/routes/authentication.route.js
+++ SourceCode/BlockchainServer/src/routes/authentication.route.js
@@ -5,6 +5,5 @@ const router = express.Router();
 
 router.route("/keypair/random").get(controller.randomKeypair);
 router.route("/keypair/email").post(controller.verifyKeyPairEmail);
-router.route("/google/token").post(controller.googleAuthentication);
 
 export default router;
