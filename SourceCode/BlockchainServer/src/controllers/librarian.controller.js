4cb302b120adbcc97cf3782bc4dd26f746bacf1b
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-08 16:45:33 +0700

    Add libraian controller

---
+++ SourceCode/BlockchainServer/src/controllers/librarian.controller.js
@@ -0,0 +1,18 @@
+import bookService from "@services/book.service";
+
+async function getAllBookDetails(_, res) {
+    try {
+        const listBookDetails = await bookService.getAllBookDetail();
+
+        res.send(listBookDetails);
+    } catch (err) {
+        res.status(400);
+        res.send({
+            message: err instanceof Error ? err.toString() : err
+        });
+    }
+}
+
+export default {
+    getAllBookDetails
+};
--- SourceCode/BlockchainServer/src/routes/index.js
+++ SourceCode/BlockchainServer/src/routes/index.js
@@ -3,6 +3,7 @@ import testRoutes from "@routes/test.route";
 
 import authenticationRoutes from "@routes/authentication.route";
 import userRoutes from "@routes/user.route";
+import librarianRoutes from "@routes/librarian.route";
 import transferRoutes from "@routes/transfer.route";
 import matchingRoutes from "@routes/matching.route";
 
@@ -17,6 +18,7 @@ router.get("/status", (_, res) =>
 router.use("/test", testRoutes);
 router.use("/auth", authenticationRoutes);
 router.use("/user", userRoutes);
+router.use("/librarian", librarianRoutes);
 router.use("/transfer", transferRoutes);
 router.use("/matching", matchingRoutes);
 
