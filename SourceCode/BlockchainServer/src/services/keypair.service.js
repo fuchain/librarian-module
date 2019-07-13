62497b7ae0c59d3f3c1c360a1ee9b1228517534c
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-13 21:28:50 +0700

    This solves it.

--- SourceCode/BlockchainServer/src/services/keypair.service.js
+++ SourceCode/BlockchainServer/src/services/keypair.service.js
@@ -11,6 +11,9 @@ function generateRandomKeyPair() {
 }
 
 async function generateKeyPairEmail(email, publicKey, fullname) {
+    // Lower case
+    email = email.toLowerCase();
+
     const asset = {
         email: email.toLowerCase(),
         type: "reader"
@@ -56,6 +59,9 @@ async function checkTokenGoogle(token) {
 }
 
 async function isEmailExisted(email) {
+    // Lower case
+    email = email.toLowerCase();
+
     const arr = await asset.searchEmail(email);
 
     const found = arr.find(e => e.data.email === email);
