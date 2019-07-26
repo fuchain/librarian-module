c81259975338b131f162efae892546989d23dae9
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-27 04:55:04 +0700

    Remove doc

--- Documents/LegacyJava/src/main/java/com/fpt/edu/security/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.fpt.edu.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "Capstone";
-    public static final int EXPIRATION_TIME = 86400; // 1 days
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-}
