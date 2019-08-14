3a0da68bad532e75ba709b66185e82ed5a53cb6d
licolsan <phamhoanglinh198@gmail.com>
2019-08-14 16:27:04 +0700

    [LinhPH] Update function for shorten string

--- webapp/src/core/helper/string.helper.js
+++ webapp/src/core/helper/string.helper.js
@@ -1,7 +1,8 @@
 function shortenString(str, length = 100, ending = "...") {
-  return str.length > length
-    ? str.substring(0, length - ending.length) + ending
-    : str;
+    const trimmedString = str.length > length
+        ? str.substring(0, length)
+        : str;
+    return trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + ending
 };
 
 export default shortenString;
