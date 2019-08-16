59c141b4189fcb5d4e01d43353168591a1f15c87
ToQuocCuong <toquoccuong55@gmail.com>
2019-08-17 01:00:17 +0700

    Fix bug login = librarian nhưng khi search thì ra duoc sach cua toi, sach dang tra

---
+++ webapp/src/views/layouts/components/navbarSearchAndPinList-librarian.js
@@ -0,0 +1,55 @@
+export default {
+  actionIcon: "StarIcon",
+  highlightColor: "warning",
+  data: [
+    {
+      index: 0,
+      label: "Thống kê",
+      url: "/librarian/statistics",
+      labelIcon: "HomeIcon",
+      highlightAction: false
+    },
+    {
+      index: 1,
+      label: "Quản lý đầu sách",
+      url: "/librarian/book-details-manage",
+      labelIcon: "BookIcon",
+      highlightAction: false
+    },
+    {
+      index: 2,
+      label: "Sách đang ghép",
+      url: "/librarian/queue",
+      labelIcon: "BookIcon",
+      highlightAction: false
+    },
+    {
+      index: 3,
+      label: "Quản lý tài khoản",
+      url: "/librarian/user-manage",
+      labelIcon: "BookIcon",
+      highlightAction: false
+    },
+    {
+      index: 4,
+      label: "Hạ tầng Swarm",
+      url: "/librarian/monitoring/infrastructure",
+      labelIcon: "BookIcon",
+      highlightAction: false
+    },
+    {
+      index: 5,
+      label: "Mạng Blockchain",
+      url: "/librarian/monitoring/bigchain",
+      labelIcon: "BookOpenIcon",
+      highlightAction: false
+    },
+    {
+      index: 7,
+      label: "Đánh giá",
+      url: "/librarian/review-report",
+      labelIcon: "ThumbsUpIcon",
+      highlightAction: false
+    }
+  ]
+};
