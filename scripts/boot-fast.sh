44a6fdd8037fee26418fa421caa4239cf514434c
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-08-23 11:29:57 +0700

    This will definitely break in 2065 (TODO)

---
+++ scripts/boot-fast.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+sudo apt install make -y
+sudo apt install wget -y
+sudo apt install unzip -y
+
+echo "Installing Docker and Docker Compose"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Installing Librarian Module"
+cd /home
+git clone https://git.fptu.tech/fuchain/librarian-module.git
+cd librarian-module
+wget -O http://builder.fptu.tech/librarian-module/docker-compose.yml
+sudo make start
+
+echo "Installing FUChain"
+cd /home
+git clone https://git.fptu.tech/fuchain/fuchain.git
+cd fuchain
+wget http://builder.fptu.tech/dump1.zip
+unzip -o dump1.zip
+rm -rf dump1.zip
+rm -rf __MACOSX
+wget -O http://builder.fptu.tech/librarian-module/docker-compose.yml
+make start
+
+echo "Import data"
+cd /home
+apt install mongo-tools -y
+wget http://builder.fptu.tech/bookdetails.json
+mongoimport -h localhost:27018 --db librarianblockchain --collection book_details --file bookdetails.json
+rm -rf bookdetails.json
+
+echo "Done deployment";
