59d2cd94c2c5951212629eed341cda1d12cf123a
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-08-23 11:18:08 +0700

    This bug has driven lots of coders completely mad. You won't believe how it ended up being fixed

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -5,11 +5,12 @@ docker tag librarian-visualizeapp-dev:latest fptudev/visualizeapp:latest
 docker tag librarian-pairworker-dev:latest fptudev/pairworker:latest
 docker tag librarian-bcserver-dev:latest fptudev/bcserver:latest
 docker tag librarian-ioserver-dev:latest fptudev/ioserver:latest
-docker tag bigchaindb_bigchaindb:latest fptudev/bigchaindb:latest
+docker tag bigchaindb_bigchaindb:latest fptudev/fuchain:latest
+docker tag fuchain_bigchaindb:latest fptudev/fuchain:latest
 
 docker push fptudev/webapp:latest
 docker push fptudev/visualizeapp:latest
 docker push fptudev/pairworker:latest
 docker push fptudev/bcserver:latest
 docker push fptudev/ioserver:latest
-docker push fptudev/bigchaindb:latest
+docker push fptudev/fuchain:latest
